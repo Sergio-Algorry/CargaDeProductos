bf9fccf87681dc7bf9fdf7e751d8059d91d6d36a
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-08-24 13:45:56 -0300

    Agregar archivos de proyecto.

---
+++ CargaDeProductos/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CargaDeProductos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(13, 13);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(52, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Código";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(16, 33);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 30);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAceptar.Location = new System.Drawing.Point(595, 33);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(95, 33);
+            this.btAceptar.TabIndex = 4;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 73);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 25);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProducto.Location = new System.Drawing.Point(122, 33);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(348, 30);
+            this.txtProducto.TabIndex = 2;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(122, 13);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(65, 17);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.Text = "Producto";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(479, 33);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 30);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(476, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Precio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 487);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CargaDeProductos/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaDeProductos
+{
+    public partial class Form1 : Form
+    {
+        string[,] productos = new string[10, 2];
+        decimal[] precios = new decimal[10];
+
+        int fila = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAceptar_Click(object sender, EventArgs e)
+        {
+            //string[,] productos = new string[10, 2];
+            //productos[0, 0] = txtCodigo.Text;
+            //productos[0, 1] = txtProducto.Text;
+
+            productos[fila, 0] = txtCodigo.Text;
+            productos[fila, 1] = txtProducto.Text;
+
+            precios[fila] = Convert.ToDecimal(txtPrecio.Text);
+
+            fila = fila + 1;
+            Listar();
+        }
+
+        private void Listar()
+        {
+            lblResultado.Text = "";
+
+            for (int i = 0; i < fila; i++)
+            {
+                lblResultado.Text = lblResultado.Text
+                    + productos[i,0]
+                    + " - "
+                    + productos[i, 1]
+                    + " - "
+                    + precios[i].ToString()
+                    + "\r\n";
+            }
+        }
+    }
+}
